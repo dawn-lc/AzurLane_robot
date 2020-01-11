d1932a09dadd956f1cc8dac60a8f3f466acd11b2
凌晨 <lc990930@163.com>
2020-01-11 21:20:35 +0800

    添加项目文件。

---
+++ AzurLane_robot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzurLane_robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
